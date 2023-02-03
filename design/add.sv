83ba7fdd11df9f0577c53c03d9f8b8a6365eba39
saswatiscaledge <saswati@scaledge.io>
2023-02-03 17:27:23 +0530

    gate design of func units

--- design/add.sv
+++ design/add.sv
@@ -5,8 +5,12 @@ module add(rs1_reg, rs2_reg, add_rd,co);
   output reg [N-1:0] add_rd;
   output reg [N-1:0] co;
   
-  always @(*) begin;
-    {co,add_rd} = rs1_reg + rs2_reg ;
+  //always @(*) begin;
+    //{co,add_rd} = rs1_reg + rs2_reg ;
+  genvar i;
+   for(i=0;i<N;i++) begin
+      xor x1(add_rd[i];rs1_reg[i],rs2_reg[i]);
+      and a1(co[i],rs1_reg[i],rs2_reg[i]);
     
     //if(co>0)
       //$display("*************exception***********");
--- design/sub.sv
+++ design/sub.sv
@@ -4,9 +4,12 @@ module sub(rs1_reg,rs2_reg,sub_rd);
 
            output reg [N-1:0] sub_rd;
   
-  always @(*) begin
-    {sub_rd} = rs1_reg - rs2_reg ;
+ // always @(*) begin
+   // {sub_rd} = rs1_reg - rs2_reg ;
   //s = a+b+ci;
   //co = a+b+ci;  
+      genvar i;
+      for(i=0;i<N;i++)begin
+          xor x2(sub_rd[i],rs1_reg[i],rs2_reg[i]);
   end
 endmodule
