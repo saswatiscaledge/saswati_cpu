8f82c9fb3b63e2aae950a9cb436ef266426ae110
saswatiscaledge <saswati@scaledge.io>
2023-02-06 11:27:47 +0530

    pc generate

---
+++ design/pc_gen.sv
@@ -0,0 +1,16 @@
+module pc(clk, rst, pc);
+input clk, rst;
+output reg [7:0] pc;
+
+always@(posedge clk)
+    begin
+        if(rst)
+            begin
+                pc <= 0;
+            end
+        else
+            begin
+                pc <= pc + 1;
+            end             
+    end
+endmodule
---
+++ design/rd_demux.sv
@@ -0,0 +1,43 @@
+module rd_demux(rd_sel,rd_val,rd_reg);//1*16
+parameter N=16;
+parameter SEL=4;
+input [SEL-1:0]rd_sel;
+input [N-1:0]rd_val;
+output reg[N-1:0]rd_reg[N-1:0];
+
+always@(*)begin
+	if(rd_sel==0)
+	rd_reg[0]=rd_val;
+	else if(rd_sel==1)
+	rd_reg[1]=rd_val;
+	else if(rd_sel==2)
+	rd_reg[2]=rd_val;
+	else if(rd_sel==3)
+	rd_reg[3]=rd_val;
+	else if(rd_sel==4)
+	rd_reg[4]=rd_val;
+	else if(rd_sel==5)
+	rd_reg[5]=rd_val;
+	else if(rd_sel==6)
+	rd_reg[6]=rd_val;
+	else if(rd_sel==7)
+	rd_reg[7]=rd_val;
+	else if(rd_sel==8)
+	rd_reg[8]=rd_val;
+	else if(rd_sel==9)
+	rd_reg[9]=rd_val;
+	else if(rd_sel==10)
+	rd_reg[10]=rd_val;
+	else if(rd_sel==11)
+	rd_reg[11]=rd_val;
+	else if(rd_sel==12)
+	rd_reg[12]=rd_val;
+	else if(rd_sel==13)
+	rd_reg[13]=rd_val;
+	else if(rd_sel==14)
+	rd_reg[14]=rd_val;
+	else if(rd_sel==15)
+	rd_reg[15]=rd_val;
+end
+endmodule
+
