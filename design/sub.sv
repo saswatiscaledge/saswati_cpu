28eae251a2187ecd32ae1183e9c7094077a2e324
saswatiscaledge <saswati@scaledge.io>
2023-02-07 17:58:13 +0530

    func units

--- design/sub.sv
+++ design/sub.sv
@@ -1,18 +1,15 @@
 module sub(rs1_reg,rs2_reg,sub_rd);
   parameter N=16;
-           input [N-1:0] rs1_reg, rs2_reg;
-
-           output reg [N-1:0] sub_rd;
-  
- // always @(*) begin
-      /* if(rs1_reg > rs2_reg)
-    		{sub_rd} = rs1_reg - rs2_reg ;
+  input [N-1:0] rs1_reg, rs2_reg;
+  output reg [N-1:0] sub_rd;
+   
+  always @(*) begin
+    if(rs1_reg > rs2_reg)begin
+    	 {sub_rd} = rs1_reg - rs2_reg ;
+	  $display("sub_rd=%p",sub_rd);
+   end
 	else
-		$display("**************rs1 < rs2,exception************");*/
-   //s = a+b+ci;
-  //co = a+b+ci;  
-      genvar i;
-      for(i=0;i<N;i++)begin
-          xor x2(sub_rd[i],rs1_reg[i],rs2_reg[i]);
-  end
+		$display("**************rs1_reg < rs2_reg,exception************");
+  
+    end
 endmodule
