200f7d6ba5f1983717fb78128a1ca48f7a8dafd7
saswatiscaledge <saswati@scaledge.io>
2023-02-07 18:51:30 +0530

    demux unit for rs2 reg

--- design/rs2_demux_1x16.sv
+++ design/rs2_demux_1x16.sv
@@ -1,25 +1,21 @@
-module rs2_demux_1x16(op_opcode,rs2_reg_val,add_rs2,sub_rs2,mul_rs2,div_rs2,and_rs2,or_rs2,xor_rs2);
+module rs2_demux_1x16(opcode,rs2_reg_val,add_rs2,sub_rs2,mul_rs2,div_rs2,and_rs2,or_rs2,xor_rs2);
   parameter N = 16;
   parameter SEL_LINE = 4;
-  input [SEL_LINE-1:0]op_opcode;
+  input [SEL_LINE-1:0]opcode;
   input [N-1:0]rs2_reg_val;
   output reg [N-1:0]add_rs2,sub_rs2,mul_rs2,div_rs2,and_rs2,or_rs2,xor_rs2;
   
   always@(*)begin
-    if(op_opcode==0)
-    add_rs2 = rs2_reg_val;
-    else if(op_opcode==1)
-    sub_rs2 = rs2_reg_val;
-    else if(op_opcode==2)
-    mul_rs2 = rs2_reg_val;
-    else if(op_opcode==3)
-    div_rs2 = rs2_reg_val;
-    else if(op_opcode==4)
-    and_rs2 = rs2_reg_val;
-    else if(op_opcode==5)
-    or_rs2 = rs2_reg_val;
-    else if(op_opcode==6)
-    xor_rs2 = rs2_reg_val;
+    case(opcode)
+    4'b0000 : add_rs2 = rs2_reg_val;
+    4'b0001 : sub_rs2 = rs2_reg_val;
+    4'b0010 : mul_rs2 = rs2_reg_val;
+    4'b0011 : div_rs2 = rs2_reg_val;
+    4'b0100 : and_rs2 = rs2_reg_val;
+    4'b0101 : or_rs2  = rs2_reg_val;
+    4'b0110 : xor_rs2 = rs2_reg_val;
+    endcase       
+    
     
   end
   
