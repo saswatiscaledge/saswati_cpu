96a2ca3477fcf6e46990616c96cd90f4e4b60014
saswatiscaledge <saswati@scaledge.io>
2023-02-16 11:45:36 +0530

    delete or file

--- design/or.sv
+++
@@ -1,8 +0,0 @@
-module or(rs1_reg,rs2_reg,or_rd);
-   parameter N=16;
-   input [N-1:0]rs1_reg,rs2_reg;
-   output reg [N-1:0]or_rd;
-   always@(*) begin
-     or_rd=rs1_reg | rs2_reg;
-   end
-endmodule
