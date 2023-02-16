d15a9f607e3420e2b4ff406acceff5d9eccb72da
saswatiscaledge <saswati@scaledge.io>
2023-02-16 10:26:47 +0530

    func units

--- design/mul.sv
+++ design/mul.sv
@@ -1,3 +1,77 @@
+//Full Adder Module
+module full_adder #(parameter N=16)
+  (
+    input [N-1:0] A,
+    input [N-1:0] B,
+    output [N-1:0] O
+);
+  
+  
+  assign  O = A+B;
+  
+endmodule
+
+//Multiplier Module
+module mul1#(parameter N=16)( 
+  input [N-1:0] rs1_reg,
+  input [N-1:0] rs2_reg,
+  output [2*N-1:0] mul_rd
+);
+ 
+//Temporary arrays to store intermediate values 
+  wire [N][  N-1:0] temp1;// [N]
+  wire [N][2*N-1:0] temp2;// [N]  
+  wire [N][2*N-1:0] temp3;// [N]
+
+//Initialize the first bit of the intermediate values  
+  //always_comb begin
+  assign	temp1[0] = rs2_reg[0] ? rs1_reg : 0;//& {N{B[0]}};
+  assign	temp2[0] = temp1[0] << 0;
+  assign	temp3[0] = temp2[0];
+  //end
+  
+  genvar i;
+  
+  generate
+    for(i=1; i<=N-1; i++)begin
+//Calculate intermediate values based on the current bit of rs2_reg
+      assign temp1[i] = rs2_reg[i] ? rs1_reg : 0;
+      assign temp2[i] = temp1[i] << i;
+//Using full adder to add intermediate values
+      full_adder#(2*N) fa(
+        .A(temp3[i-1]),
+        .B(temp2[i]),
+        .O(temp3[i])
+      );
+    end
+  endgenerate
+
+  assign mul_rd = temp3[N-1];
+  
+endmodule
+ 
+
+
+//16bit Multiplier using HA and FA
+/*module HA(s,c,a,b);
+  output s,c;
+  input a,b;
+  xor x1(s,a,b);
+  and c1(c,a,b);
+endmodule
+
+module FA(s,c,a,b,cin);
+  output s,c;
+  input a,b,cin;
+  xor x2(s,a,b,cin);
+  and c2(y1,a,b);
+  and c3(y2,a,cin);
+  and c4(y3,b,cin);
+  or o1(c,c2,c3,c4);
+  //assign s=(a^b^cin);
+ // assign c=((a&b)|(a&cin)|(b&cin));
+endmodule
+
 module mul(rs1_reg,rs2_reg,mul_rd);
   parameter N=4;
   parameter M=8;
@@ -40,6 +114,7 @@ module mul(rs1_reg,rs2_reg,mul_rd);
   FA FA7(mul_rd[5],x10,x9,w[14],x11);
   FA FA6(mul_rd[6],mul_rd[7],x8,w[15],x10);
     
+endmodule*/
 
  /* HA HA1(mul_rd[1],x1,(rs1_reg[1]&rs2_reg[0]),(rs1_reg[0]&rs2_reg[1]));
  
@@ -56,75 +131,10 @@ module mul(rs1_reg,rs2_reg,mul_rd);
   FA FA8(mul_rd[4],x11,x13,(rs1_reg[3]&rs2_reg[1]),x12);
   FA FA7(mul_rd[5],x10,x9,(rs1_reg[3]&rs2_reg[2]),x11);
   FA FA6(mul_rd[6],mul_rd[7],x8,(rs1_reg[3]&rs2_reg[3]),x10);*/
-endmodule
 
 
-module HA(s,c,a,b);
-  output s,c;
-  input a,b;
-  xor x1(s,a,b);
-  and c1(c,a,b);
-endmodule
 
-module FA(s,c,a,b,cin);
-  output s,c;
-  input a,b,cin;
-  xor x2(s,a,b,cin);
-  and c2(y1,a,b);
-  and c3(y2,a,cin);
-  and c4(y3,b,cin);
-  or o1(c,c2,c3,c4);
-  //assign s=(a^b^cin);
- // assign c=((a&b)|(a&cin)|(b&cin));
-endmodule*/
   
 
-/*module full_adder #(parameter N=16)
-  (
-    input [N-1:0] A,
-    input [N-1:0] B,
-    output [N-1:0] O
-);
-  
-  
-  assign  O = A+B;
-  
-endmodule
-
-module mul#(parameter N=16)( 
-  input [N-1:0] rs1_reg,
-  input [N-1:0] rs2_reg,
-  output [2*N-1:0] mul_rd
-);
-  
-  wire [N][  N-1:0] temp1;// [N];
-  wire [N][2*N-1:0] temp2;// [N];
-  
-  wire [N][2*N-1:0] temp3;// [N];
-  
-  //always_comb begin
-  assign	temp1[0] = rs2_reg[0] ? rs1_reg : 0;//& {N{B[0]}};
-  assign	temp2[0] = temp1[0] << 0;
-  assign	temp3[0] = temp2[0];
-  //end
-  
-  genvar i;
-  
-  generate
-    for(i=1; i<=N-1; i++)begin
-      assign temp1[i] = rs2_reg[i] ? rs1_reg : 0;
-      assign temp2[i] = temp1[i] << i;
-      full_adder#(2*N) fa(
-        .rs1_reg(temp3[i-1]),
-        .rs2_reg(temp2[i]),
-        .mul_rd(temp3[i])
-      );
-    end
-  endgenerate
-
-  assign mul_rd = temp3[N-1];
-  
-endmodule*/
- 
   
   
