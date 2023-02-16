8b47691203623a6f4d590f50d07b17203b65deff
saswatiscaledge <saswati@scaledge.io>
2023-02-16 17:24:18 +0530

    xor instruction

---
+++ design/xor_op.sv
@@ -0,0 +1,10 @@
+module xor_op(rs1_reg,rs2_reg,xor_rd);
+   parameter N=16;
+   input [N-1:0]rs1_reg,rs2_reg;
+   output reg [N-1:0]xor_rd;
+
+   always@(*) begin
+     xor_rd=rs1_reg ^ rs2_reg;
+   end
+
+endmodule
