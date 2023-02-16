00123600b902e877fc1d06e71de165cdf13eec76
saswatiscaledge <saswati@scaledge.io>
2023-02-16 13:32:51 +0530

    func units

--- design/div.sv
+++ design/div.sv
@@ -1,52 +1,73 @@
-module full_subtractor #(parameter N=16)
-  (
-    input [N-1:0] A,
-    input [N-1:0] B,
-    output [N-1:0] Diff,
-    output Borrow
-  );
-  
-  wire [N-1:0] temp;
-  
-  assign temp = A - B;
-  assign Diff = temp;
-  assign Bor = (A < B);
-  
+module div(rs1_reg,rs2_reg,remainder,rd_div);
+parameter N=16;
+input [N-1:0] rs1_reg,rs2_reg;
+output reg [N-1:0] remainder,rd_div;
+
+// Variables
+integer i;
+  reg [N-1:0] divisor_copy, dividend_copy;
+  reg [N-1:0] temp;
+
+always @(rs1_reg or rs2_reg)
+begin
+	divisor_copy = rs2_reg;
+	dividend_copy = rs1_reg;
+	temp = 0; 
+        
+ 
+ 
+  for(i = 0;i < N;i = i + 1)
+	begin
+      temp = {temp[N-2:0], dividend_copy[N-1]};
+      dividend_copy[N-1:1] = dividend_copy[N-2:0];
+		/*
+			* Substract the Divisor Register from the Remainder Register and
+			* plave the result in remainder register (temp variable here!)
+		*/
+		temp = temp - divisor_copy;
+		// Compare the Sign of Remainder Register (temp)
+      if(temp[N-1] == 1)
+		begin
+		/*
+			* Restore original value by adding the Divisor Register to the
+			* Remainder Register and placing the sum in Remainder Register.
+			* Shift Quatient by 1 and Add 0 to last bit.
+		*/
+			dividend_copy[0] = 0;
+			temp = temp + divisor_copy;
+		end
+		else
+		begin
+		/*
+			* Shift Quatient to left.
+			* Set right most bit to 1.
+		*/
+			dividend_copy[0] = 1;
+		end
+	end
+	rd_div = dividend_copy;
+	remainder = temp;//dividend - (divisor_copy*dividend_copy)
+end
+ 
 endmodule
 
-module divider#(parameter N=16)( 
-  input [N-1:0] rs1_reg,//rs1_reg=dividend
-  input [N-1:0] rs2_reg,//rs2_reg=divisor
-  output reg[N-1:0] rd1_quotient,
-  output reg[N-1:0] rd2_remainder
-);
-  
-  wire [N-1:0] temp_remainder;
-  wire [N-1:0] current_dividend;
-  wire quotient_bit;
-  
 
-  initial begin
-    temp_remainder = rs1_reg;
-    rd1_quotient = 0;
-  end
-  
-  genvar i;
-  
-  generate
-    for(i=N-1; i>=0; i=i-1)begin
-      current_dividend == {temp_remainder[N-1:i], rd1_quotient[i-1:0]};  //The result of the concatenation operation is a new N-bit value current_dividend with the higher N-i bits taken from temp_remainder and the lower i bits taken from Quotient. 
-      full_subtractor#(N) fs(
-        .A(current_dividend),
-        .B(rs2_reg),
-        .Diff(temp_remainder),
-        .Borrow(quotient_bit)
-      );
-      assign rs1_reg[i] = quotient_bit;
-    end
-  endgenerate
 
-  assign rd2_remainder = temp_remainder;
-  
-endmodule
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
